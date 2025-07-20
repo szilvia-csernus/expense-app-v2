60ad3f3e2b53fde25d117c88f9fef3343591c905
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-07-20 09:24:17 +0100

    refactor: comment out unused Amplify imports in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
-import { Amplify } from 'aws-amplify';
-import outputs from '../amplify_outputs.json';
+// import { Amplify } from 'aws-amplify';
+// import outputs from '../amplify_outputs.json';
 
-Amplify.configure(outputs)
+// Amplify.configure(outputs)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
