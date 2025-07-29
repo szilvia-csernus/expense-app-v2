92cf0f4ff2a554e514879fa7347b9d92ff478bd5
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-07-29 17:56:00 +0100

    style: format code for consistency in index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit';
-import costFormSlice from './cost-form-slice';
-import thankYouMessageSlice from './thank-you-message-slice';
-import errorMessageSlice from './error-message-slice';
-import churchSlice from './church-slice';
+import { configureStore } from "@reduxjs/toolkit";
+import costFormSlice from "./cost-form-slice";
+import thankYouMessageSlice from "./thank-you-message-slice";
+import errorMessageSlice from "./error-message-slice";
+import churchSlice from "./church-slice";
 
-import { useDispatch, useSelector, type TypedUseSelectorHook } from 'react-redux';
-
+import {
+  useDispatch,
+  useSelector,
+  type TypedUseSelectorHook,
+} from "react-redux";
 
 const store = configureStore({
-	reducer: {
-		church: churchSlice.reducer,
-		costForm: costFormSlice.reducer,
-		thankYouMessage: thankYouMessageSlice.reducer,
-		errorMessage: errorMessageSlice.reducer,
-	}
+  reducer: {
+    church: churchSlice.reducer,
+    costForm: costFormSlice.reducer,
+    thankYouMessage: thankYouMessageSlice.reducer,
+    errorMessage: errorMessageSlice.reducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
