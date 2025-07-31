9d543e095a934c153c244fc81e1aa4345ff2ad61
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-07-31 17:10:55 +0100

    fix: update sendExpenseForm configuration to include environment and runtime settings

--- amplify/functions/sendExpenseForm/resource.ts
+++ amplify/functions/sendExpenseForm/resource.ts
@@ -6,4 +6,9 @@ export const sendExpenseForm = defineFunction({
   // optional - defaults to "./handler.ts" anyway
   entry: "./handler.ts",
   timeoutSeconds: 30, // Increase from 3s to 30s
+  environment: {
+    AMPLIFY_DATA_DEFAULT_NAME: "main",
+  },
+  runtime: 22, // Use Node.js 22 runtime
+  resourceGroupName: "data", // Optional - groupping this function with the data resource
 });
