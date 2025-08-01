5d9534049d4a1e6966a4787d08a78a62f297c245
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-08-01 13:40:41 +0100

    refactor: rename receiptsBucket to storage and update access permissions

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,13 +1,13 @@
 import { defineStorage } from "@aws-amplify/backend";
 import { sendExpenseForm } from "../functions/sendExpenseForm/resource";
 
-export const receiptsBucket = defineStorage({
-  name: "receiptsBucket",
+export const storage = defineStorage({
+  name: "expenses",
   access: (allow) => ({
     "receipts/*": [
       allow.guest.to(["read", "write", "delete"]),
       // allow.authenticated.to(["read", "write", "delete"]),
-      allow.resource(sendExpenseForm).to(["read"]),
+      allow.resource(sendExpenseForm).to(["read", "delete"]),
     ],
   }),
 });
