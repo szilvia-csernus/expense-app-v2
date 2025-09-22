ada4eb2e388a6bd3b3bc97afa79ac9eba4e4c076
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-09-22 18:03:47 +0100

    feat: refactor Modal component and add ModalMessage for displaying messages

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -1,50 +1,45 @@
-import ReactDOM from 'react-dom';
-import { thankYouMessageActions } from '../store/thank-you-message-slice';
-import { errorMessageActions } from '../store/error-message-slice';
-import { useAppDispatch, useAppSelector } from '../store/index';
+import ReactDOM from "react-dom";
+import { modalMessageActions } from "../store/modal-message-slice";
+import { useAppDispatch, useAppSelector } from "../store/index";
 
-import classes from './Modal.module.css';
+import classes from "./Modal.module.css";
 
 type ModalProps = {
-	children?: React.ReactNode;
+  children?: React.ReactNode;
 };
 
 const Backdrop = () => {
-	const thankYouMessage = useAppSelector((state) => state.thankYouMessage);
-	const errorMessage = useAppSelector((state) => state.errorMessage.status);
+  const modalMessage = useAppSelector((state) => state.modalMessage);
+  const dispatch = useAppDispatch();
 
-	const dispatch = useAppDispatch();
-	
-	const clickHandler = () => {
-		if (thankYouMessage) dispatch(thankYouMessageActions.close());
-		if (errorMessage) dispatch(errorMessageActions.close());
-	};
+  const clickHandler = () => {
+    if (modalMessage) dispatch(modalMessageActions.close());
+  };
 
-	return <div className={classes.backdrop} onClick={clickHandler}/>;
+  return <div className={classes.backdrop} onClick={clickHandler} />;
 };
 
 const Overlay = (props: ModalProps) => {
-	return <div className={classes.overlay}>{props.children}</div>;
+  return <div className={classes.overlay}>{props.children}</div>;
 };
 
 const Canvas = (props: ModalProps) => {
-	return <div className={classes.canvas}>{props.children}</div>;
+  return <div className={classes.canvas}>{props.children}</div>;
 };
 
-
 const Modal = (props: ModalProps) => {
-	const portalElement = document.getElementById('overlay')!;
-	return (
-		<>
-			{ReactDOM.createPortal(<Backdrop />, portalElement)}
-			{ReactDOM.createPortal(
-				<Overlay>
-					<Canvas>{props.children}</Canvas>
-				</Overlay>,
-				portalElement
-			)}
-		</>
-	);
+  const portalElement = document.getElementById("overlay")!;
+  return (
+    <>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <Overlay>
+          <Canvas>{props.children}</Canvas>
+        </Overlay>,
+        portalElement
+      )}
+    </>
+  );
 };
 
 export default Modal;
