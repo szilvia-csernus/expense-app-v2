289561bf33e8d973da74d53485038b87615f2feb
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-09-22 15:32:44 +0100

    feat: update storage configuration to use expenseAppStorage and adjust access rules for logos

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,13 +1,12 @@
 import { defineStorage } from "@aws-amplify/backend";
-import { sendExpenseForm } from "../functions/sendExpenseForm/resource";
 
 export const storage = defineStorage({
-  name: "expenses",
+  name: "expenseAppStorage",
+  isDefault: true,
   access: (allow) => ({
-    "receipts/*": [
-      allow.guest.to(["read", "write", "delete"]),
-      // allow.authenticated.to(["read", "write", "delete"]),
-      allow.resource(sendExpenseForm).to(["read", "delete"]),
+    "logos/*": [
+      allow.authenticated.to(["read", "write", "delete"]),
+      allow.guest.to(["get"]),
     ],
   }),
 });
