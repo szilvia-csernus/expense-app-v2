85b2919ede0139010b950810879a80e8336b00f0
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-09-22 17:55:07 +0100

    feat: refactor and extend button components with improved styling and additional props

--- src/Components/Buttons.tsx
+++ src/Components/Buttons.tsx
@@ -1,67 +1,87 @@
-import classes from './Buttons.module.css';
+import classes from "./Buttons.module.css";
 
-interface ButtonProps {
-	onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
-	children: React.ReactNode;
-}
-
-type GeneralButtonProps = ButtonProps & {
-	type: 'button' | 'submit' | 'reset';
-	className: string;
+type ButtonProps = {
+  type?: "button" | "submit" | "reset";
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+  className?: string;
+  children: React.ReactNode;
 };
 
-export const GeneralButton = (props: GeneralButtonProps) => {
-	const classNames = `${classes.button} ${props.className}`;
-	return (
-		<button type={props.type} className={classNames} onClick={props.onClick}>
-			{props.children}
-		</button>
-	);
-};
-
-export const SecondaryButton = (props: ButtonProps) => {
-	return (
-		<GeneralButton
-			type="button"
-			className={classes.secondaryButton}
-			onClick={props.onClick}
-		>
-			{props.children}
-		</GeneralButton>
-	);
-};
-
-export const SubmitButton = (props: ButtonProps) => {
-	return (
-		<GeneralButton
-			type="submit"
-			className={classes.primaryButton}
-		>
-			{props.children}
-		</GeneralButton>
-	);
-};
-
-export const DeleteButton = (props: ButtonProps) => {
-	return (
-		<GeneralButton
-			type="button"
-			className={classes.deleteButton}
-			onClick={props.onClick}
-		>
-			{props.children}
-		</GeneralButton>
-	);
+export const GeneralButton = (props: ButtonProps) => {
+  const classNames = `${classes.button} ${props.className}`;
+  return (
+    <button
+      type={props.type || "button"}
+      className={classNames}
+      disabled={props.disabled || false}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
 };
 
 export const PrimaryButton = (props: ButtonProps) => {
-	return (
-		<GeneralButton
-			type="button"
-			className={classes.primaryButton}
-			onClick={props.onClick}
-		>
-			{props.children}
-		</GeneralButton>
-	);
+  return (
+    <GeneralButton
+      type={props.type || "button"}
+      className={classes.primaryButton}
+      onClick={props.onClick}
+      disabled={props.disabled || false}
+    >
+      {props.children}
+    </GeneralButton>
+  );
+};
+
+export const SecondaryButton = (props: ButtonProps) => {
+  return (
+    <GeneralButton
+      type="button"
+      className={classes.secondaryButton}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </GeneralButton>
+  );
+};
+
+export const DeleteButton = (props: ButtonProps) => {
+  return (
+    <GeneralButton
+      type="button"
+      className={classes.deleteButton}
+      onClick={props.onClick}
+      disabled={props.disabled || false}
+    >
+      {props.children}
+    </GeneralButton>
+  );
+};
+
+export const EditButton = (props: ButtonProps) => {
+  return (
+    <GeneralButton
+      type={props.type || "button"}
+      className={classes.editButton}
+      onClick={props.onClick}
+      disabled={props.disabled || false}
+    >
+      {props.children}
+    </GeneralButton>
+  );
+};
+
+export const EditIconButton = (props: ButtonProps) => {
+  return (
+    <GeneralButton
+      type={props.type || "button"}
+      className={`${classes.editButton} ${classes.editIconButton}`}
+      onClick={props.onClick}
+      disabled={props.disabled || false}
+    >
+      {props.children}
+    </GeneralButton>
+  );
 };
