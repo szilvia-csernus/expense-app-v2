bd9c55fa76ee48e4e4bba9520f9a7b0c87cc451e
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-10-19 17:52:21 +0100

    feat: Enhance email verification and reset password UI components

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,6 +6,35 @@ import { defineAuth } from "@aws-amplify/backend";
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      verificationEmailStyle: "CODE",
+      verificationEmailSubject: "Expense App Admin - Verify your email",
+      verificationEmailBody: (createCode) =>
+        `<div>
+          <div style="font-size: 1.5em; font-weight: bold;">
+            Verify your email address for Expense App Admin
+          </div>
+          <br><br><br>
+          Here is the code to confirm your account:
+          <div style="font-size: 1.5em; color: #a647f9ff;">${createCode()}</div>
+          </div>`,
+      userInvitation: {
+        emailSubject: "Expense App Admin - Complete your registration",
+        emailBody: (user, code) =>
+          `<div>
+        <div style="font-size: 1.5em; font-weight: bold;">
+            Complete your registration for Expense App Admin
+          </div>
+          <br><br><br>
+          You can now log in with your email ${user()} and temporary password:
+          <br><br>
+          <div style="font-size: 1.5em; color: #a647f9ff;">${code()}</div>
+          <br><br><br>
+          You will be prompted to change your password after logging in.
+          <br><br>
+          Please note, this temporary password will expire in 7 days.
+          </div>`,
+      },
+    },
   },
 });
