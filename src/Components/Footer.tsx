49a830f5b811353e328a73ca02d04511b158311f
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-10-20 14:23:55 +0100

    feat: Update logo asset format from PNG to WEBP in Footer component

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import classes from "./Footer.module.css";
-import logoUrl from "../images/expLogo.png";
+import logoUrl from "../images/expLogo.webp";
 
 function Footer({ children }: { children?: React.ReactNode }) {
   return (
