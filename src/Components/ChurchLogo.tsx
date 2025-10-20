5015276f524e23d4fafad28a254e8514401d1d69
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-10-20 14:24:33 +0100

    feat: Update fallback image format from PNG to WEBP in ChurchLogo component

--- src/Components/ChurchLogo.tsx
+++ src/Components/ChurchLogo.tsx
@@ -8,10 +8,13 @@ const ChurchLogo = () => {
 
   return (
     <StorageImage
-      path={churchLogo || "logos/Image_not_available.png"}
+      path={churchLogo || "logos/Image_not_available.webp"}
       className={classes.churchLogo}
       alt="church logo"
       style={{ height: "60px", width: "auto" }}
+      loading="eager"
+      width={202} // explicit width and height for aspect ratio to satisfy LightHouse
+      height={60}
     />
   );
 };
