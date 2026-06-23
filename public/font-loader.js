a0dbb0ae32f6dd64a69887af2f1289ff5853fb93
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2026-06-23 20:05:05 +0100

    fix blocked 'inline onload' CSP issue

--- public/font-loader.js
+++ public/font-loader.js
@@ -1,3 +1,9 @@
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.href =
+  "https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,500;1,500&family=Quicksand:wght@300..700&display=swap";
+document.head.appendChild(link);
+
 if ("fonts" in document) {
   Promise.all([
     document.fonts.load('500 1em "Josefin Sans"'),
